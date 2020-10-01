2e8bafffb98dc9e408972bf6912989c301491fb9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-10-01 16:18:40 +0800

    开发环境不压缩 JS/CSS

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -21,6 +21,7 @@ module.exports = {
     rules,
   },
   optimization: {
+    minimize: process.env.NODE_ENV !== 'development',
     minimizer: [
       new TerserJSPlugin({}),
       new OptimizeCSSAssetsPlugin({}),
